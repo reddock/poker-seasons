41f54ccdacc9f39a2dde55c31c228a2ef920cd6b
Mike <michael.reddock+git@gmail.com>
2020-08-03 16:43:34 -0500

    Extra stuff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="red" dark>
+      <v-btn href="http://mikespokernight.com" target="_blank" text>
+        <span class="mr-2">Mike's Poker Night</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <SeasonStandings />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SeasonStandings from "./components/SeasonStandings";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    SeasonStandings,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
