6e01bc2b7e8b839babebd93db322d03100e08aeb
Mike <michael.reddock+git@gmail.com>
2021-02-02 11:53:29 -0600

    New Season Game 1

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,10 +4,16 @@
       <v-row align="alignment" :justify="justify" class="ma-1 pa-1">
         <v-col>
           <v-tabs>
+            <v-tab>Feb/Mar</v-tab>
             <v-tab>Dec/Jan</v-tab>
             <v-tab>November</v-tab>
             <v-tab>October</v-tab>
             <v-tab>September</v-tab>
+            <v-tab-item>
+              <v-card flat>
+                <FebMarMerit />
+              </v-card>
+            </v-tab-item>
             <v-tab-item>
               <v-card flat>
                 <DecJanMerit />
@@ -50,6 +56,7 @@ import SeptMerit from "./SeptMerit";
 import OctMerit from "./OctMerit";
 import NovMerit from "./NovMerit";
 import DecJanMerit from "./DecJanMerit";
+import FebMarMerit from "./FebMarMerit";
 
 export default {
   name: "Dashboard",
@@ -61,6 +68,7 @@ export default {
     OctMerit,
     NovMerit,
     DecJanMerit,
+    FebMarMerit,
     PointSystem,
     Blinds,
   },
